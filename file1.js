fb2fa1b86ac0a4128fe30e47c05fdcee8f27407a
zhansingsong <xingxiang.zhan@pingcap.com>
2022-10-25 14:21:57 +0800

    add

--- file1.js
+++ file1.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 console.log('vvv');
 console.log('new line')
 console.log('vvv111');
 console.log('ddd');
-=======
-<<<<<<< HEAD
-console.log('vvv111');
-console.log('ddd');
-=======
-console.log('vvv');
-console.log('new line')
->>>>>>> b221aa6 (add feat2)
->>>>>>> 45a9c5e (add feat2)
